40dcd28a98921047c5c41bc02be251ec043cf5b8
wells86 <calebrwells@gmail.com>
2017-08-22 21:45:48 -0700

    Did not get much done on day one. I will do more tomorrow.

--- 100-Days-Of-Code.playground/Contents.swift
+++ 100-Days-Of-Code.playground/Contents.swift
@@ -2,4 +2,45 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// Day 001
+
+var sleep = 8
+
+func sleepy() {
+    if sleep < 8 {
+        print("Get more sleep!")
+    } else {
+        print("Good job!")
+    }
+}
+
+sleepy()
+
+func time() {
+    let week = 168
+    let workDay = 8
+    //let OneHundredDaysOfCode = 1
+    
+    let workWeek = workDay * 5
+    let weeklySleep = sleep * 7
+    let weeklySchedule = weeklySleep + workWeek
+    
+    if weeklySchedule < week {
+        let myWeek = week - weeklySchedule
+        print(myWeek)
+        print(myWeek / 7)
+    }
+}
+
+time()
+
+struct Food {
+    var name: String
+    var ExpirationDate: Int
+}
+
+// Day 002
+
+
+
+
