218f4a561177abc7be78efc75eb10667625135b0
wells86 <calebrwells@gmail.com>
2017-08-25 21:49:46 -0700

    Playing with translation in the matrix

--- Game AR/Game AR/Scene.swift
+++ Game AR/Game AR/Scene.swift
@@ -21,9 +21,11 @@ class Scene: SKScene {
         // Create anchor using the camera's current position
         if let currentFrame = sceneView.session.currentFrame {
             
-            // Create a transform with a translation of 0.2 meters in front of the camera
+            // Create a transform with a translation
             var translation = matrix_identity_float4x4
             translation.columns.3.z = -9
+            translation.columns.3.y = -3
+            translation.columns.3.x = -3
             let transform = simd_mul(currentFrame.camera.transform, translation)
             
             // Add a new anchor to the session
