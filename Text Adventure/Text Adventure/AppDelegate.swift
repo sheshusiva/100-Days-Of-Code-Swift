607fa70c3caf13ad8a4b6263cb4af8e450920c8d
wells86 <calebrwells@gmail.com>
2017-08-28 22:17:09 -0700

    ...

--- Text Adventure/Text Adventure/AppDelegate.swift
+++ Text Adventure/Text Adventure/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        let vc = ViewController(collectionViewLayout: UICollectionViewFlowLayout())
+        window?.rootViewController = UINavigationController(rootViewController: vc)
         return true
     }
 
