6f496c4dcb1f0b763c99fe95d6c0e10b4bd49d85
wells86 <calebrwells@gmail.com>
2017-09-06 20:26:10 -0700

    Added a target and removed the gun.

--- Game AR/Game AR/Scene.swift
+++ Game AR/Game AR/Scene.swift
@@ -21,7 +21,7 @@ var score = 0 {
     }
 }
 
-let gunTexture = SKTexture(imageNamed: "Shoot_F01")
+let gunTexture = SKTexture(imageNamed: "Gun_Scope")
 var gunShooting: SKSpriteNode = SKSpriteNode()
 var gunSprite: SKSpriteNode = SKSpriteNode()
 
@@ -70,7 +70,7 @@ class Scene: SKScene {
     }
     
     override func didMove(to view: SKView) {
-        gun(1)
+        gun()
         gameCenterIcon()
         createScore()
     }
@@ -111,7 +111,7 @@ class Scene: SKScene {
         let point = touch.location(in: self.view)
         
         if point.x > size.width / 2 && point.y < size.width / 2 {
-            gunShoooting(1)
+            gun()
 //            print("== Duck X position is: \(xTranslation)")
 //            print("== Duck Y position is: \(yTranslation)")
 //            print("== Gun position is: \(gunShooting.position)")
@@ -122,45 +122,15 @@ class Scene: SKScene {
                 sprite.removeFromParent()
                 
             }
-            gun(0)
         }
     }
 
-    func gun(_ number: Int) {
-        if number == 1 {
-            let gunTexture = SKTexture(imageNamed: "Shoot_F01")
-            gunSprite = SKSpriteNode(texture:gunTexture)
+    func gun() {
+        let gunTexture = SKTexture(imageNamed: "Gun_Scope")
+        gunSprite = SKSpriteNode(texture:gunTexture)
 
-            gunSprite.scale(to: CGSize(width: 250, height: 250))
-            gunSprite.position = CGPoint(x: 0, y: (view?.frame.midY)! - 320)
-            gunSprite.zPosition = -50
-            addChild(gunSprite)
-        }
-    }
-
-    func gunShoooting(_ number: Int) {
-
-        gunShooting = SKSpriteNode(texture:gunTexture)
-
-        if number == 1 {
-            print("===== BANG!!! BANG!!!")
-
-            gunShooting.scale(to: CGSize(width: 250, height: 250))
-            gunShooting.position = CGPoint(x: 0, y: (view?.frame.midY)! - 300)
-
-            let frame2 = SKTexture(imageNamed: "Shoot_F02")
-            let frame3 = SKTexture(imageNamed: "Shoot_F03")
-//            let frame4 = SKTexture(imageNamed: "Shoot_F04")
-//            let frame5 = SKTexture(imageNamed: "Shoot_F05")
-//            let frame6 = SKTexture(imageNamed: "Shoot_F06")
-
-            let animation = SKAction.animate(with: [gunTexture, frame2, frame3, gunTexture], timePerFrame: 0.2)
-
-            gunShooting.run(animation)
-
-            gunShooting.zPosition = 72
-
-            self.addChild(gunShooting)
-        }
+        gunSprite.scale(to: CGSize(width: 150, height: 150))
+        gunSprite.position = CGPoint(x: 0, y: 0)
+        addChild(gunSprite)
     }
 }
