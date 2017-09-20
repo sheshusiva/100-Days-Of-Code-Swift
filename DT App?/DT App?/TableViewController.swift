ebab83afb6b235486293cd8e908980bdba5ae4db
wells86 <calebrwells@gmail.com>
2017-09-19 22:42:23 -0700

    Playing with leadingSwipeActionsConfigurationForRowAt.

--- DT App?/DT App?/TableViewController.swift
+++ DT App?/DT App?/TableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
+    let cellId = "cellId"
+    
     override func prefersHomeIndicatorAutoHidden() -> Bool {
         return true
     }
@@ -20,8 +22,9 @@ class TableViewController: UITableViewController {
         navigationItem.title = "Daily Travels"
         
         navigationItem.rightBarButtonItem = UIBarButtonItem.init(barButtonSystemItem: .add, target: self, action: #selector(addButton))
-        
         navigationItem.leftBarButtonItem = UIBarButtonItem.init(barButtonSystemItem: .edit, target: self, action: #selector(editButton))
+        
+        tableView.register(TableViewCellLayout.self, forCellReuseIdentifier: cellId)
     }
     
     @objc func addButton() {
@@ -31,6 +34,33 @@ class TableViewController: UITableViewController {
     @objc func editButton() {
         //
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+    }
+    
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let actionOne = UIContextualAction(style: .normal, title: "Action One") {
+            action, view, completionHandler in
+            print("Marking read")
+            completionHandler(true)
+        }
+        
+        let actionTwo = UIContextualAction(style: .normal, title: "Action Two") {
+            action, view, completionHandler in
+            print("Marking read")
+            completionHandler(true)
+        }
+        
+        actionOne.backgroundColor = UIColor.blue
+        actionTwo.backgroundColor = UIColor.magenta
+        
+        return UISwipeActionsConfiguration(actions: [actionOne, actionTwo])
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -38,5 +68,29 @@ class TableViewController: UITableViewController {
     }
 
 
+}
+
+class TableViewCellLayout: UITableViewCell {
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupSubViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let cellLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Test label"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupSubViews() {
+        addSubview(cellLabel)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": cellLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": cellLabel]))
+    }
 }
 
