555871065d89f757ab6fa16f4a8490b6a021ee71
wells86 <calebrwells@gmail.com>
2017-09-22 20:26:44 -0700

    Created a Bicycle class with the superclass of Vehicle.

--- Classes & Inheritance.playground/Contents.swift
+++ Classes & Inheritance.playground/Contents.swift
@@ -17,3 +17,31 @@ class Person {
 let person = Person(name: "Jay")
 print(person.name)
 person.sayHello()
+
+// new class
+
+class Vehicle {
+    var currentSpeed = 0.0
+    
+    var description: String {
+        return "traveling at \(currentSpeed) miles per hour."
+    }
+    
+    func makeNoise() {
+        // do nothing
+    }
+}
+
+let someVehicle = Vehicle()
+print("Vehicle: \(someVehicle.description)")
+
+//  The Bicycle class with the superclass of Vehicle.
+/*
+The new Bicycle class automatically inherits all of the properties and
+methods of Vehicle, such as its currentSpeed and description
+properties and its makeNoise() method.
+ */
+
+class Bicycle: Vehicle {
+    var hasBasket = false
+}
