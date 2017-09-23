f0c2931ff48e19f2ce3cfde3067b9f42c9e4a3cc
wells86 <calebrwells@gmail.com>
2017-09-22 20:13:12 -0700

    Wrote a simple class with a constant and a function.

--- Classes & Inheritance.playground/Contents.swift
+++ Classes & Inheritance.playground/Contents.swift
@@ -2,3 +2,18 @@
 
 import UIKit
 
+class Person {
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func sayHello() {
+        print("Hello, there!")
+    }
+}
+
+let person = Person(name: "Jay")
+print(person.name)
+person.sayHello()
