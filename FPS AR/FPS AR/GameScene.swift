44bb3e782b9d02276d84ed1c06e64764c62e411d
wells86 <calebrwells@gmail.com>
2017-09-26 21:33:50 -0700

    Shooting targets.

--- FPS AR/FPS AR/GameScene.swift
+++ FPS AR/FPS AR/GameScene.swift
@@ -14,6 +14,7 @@ class GameScene: SKScene {
     }
     
     var isWorldSetUp = false
+    var gun: SKSpriteNode!
     
     private func setUpWorld() {
         guard let currentFrame = sceneView.session.currentFrame else {
@@ -49,5 +50,49 @@ class GameScene: SKScene {
                 target.colorBlendFactor = blendFactor
             }
         }
+    }
+    
+    override func didMove(to view: SKView) {
+        gun = SKSpriteNode(imageNamed: "Gun_F01")
+        gun.position = CGPoint(x: (view.frame.midX) / 8, y: (view.frame.midY) - 340)
+        addChild(gun)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let location = gun.position
+        let hitNodes = nodes(at: location)
+        
+        var hitTarget: SKNode?
+        for node in hitNodes {
+            if node.name == "target" {
+                hitTarget = node
+                break
+            }
+        }
+        
+        if let hitTarget = hitTarget, let anchor = sceneView.anchor(for: hitTarget) {
+            let hit = SKAction.playSoundFileNamed("quack", waitForCompletion: false)
+            
+            let action = SKAction.run {
+                self.sceneView.session.remove(anchor: anchor)
+            }
+            
+            let group = SKAction.group([hit, action])
+            let sequence = [SKAction.wait(forDuration: 0.3), group]
+            hitTarget.run(SKAction.sequence(sequence))
+            
+        }
+        
+        let bang = SKAction.playSoundFileNamed("gun-shot", waitForCompletion: false)
+        let frame1 = SKTexture(imageNamed: "Gun_F01")
+        let frame2 = SKTexture(imageNamed: "Gun_F02")
+        let frame3 = SKTexture(imageNamed: "Gun_F03")
+        
+        let animation = SKAction.animate(with: [frame2, frame3, frame1], timePerFrame: 0.3)
+        
+        let group = SKAction.group([bang, animation])
+        
+        gun.run(group)
+        
     }
 }
