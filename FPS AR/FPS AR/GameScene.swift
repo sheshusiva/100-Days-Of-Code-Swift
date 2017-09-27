30f954c5ac275e8fffd9a025a43848ca6859e878
wells86 <calebrwells@gmail.com>
2017-09-26 19:04:05 -0700

    Starting a new FPS AR game! : )

---
+++ FPS AR/FPS AR/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  FPS AR
+//
+//  Created by Caleb Wells on 9/26/17.
+//  Copyright © 2017 Caleb Wells. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+}
---
+++ FPS AR/FPS AR/GameViewController.swift
@@ -0,0 +1,76 @@
+//
+//  GameViewController.swift
+//  FPS AR
+//
+//  Created by Caleb Wells on 9/26/17.
+//  Copyright © 2017 Caleb Wells. All rights reserved.
+//
+
+import ARKit
+
+class GameViewController: UIViewController {
+    
+    var sceneView: ARSKView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let view = self.view as? ARSKView {
+            sceneView = view
+            sceneView!.delegate = self
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .resizeFill
+            scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+            view.presentScene(scene)
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .landscapeRight
+        } else {
+            return .all
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
+// MARK: === Respond to session events
+extension GameViewController: ARSKViewDelegate {
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        print("Session failed")
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        print("Session was interrupted")
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        print("Session resumed")
+        sceneView.session.run(session.configuration!, options: [.resetTracking, .removeExistingAnchors])
+    }
+}
