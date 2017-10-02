1f7803ddd4a7ece1c5563cb17c6559ec3a9a64b7
wells86 <calebrwells@gmail.com>
2017-10-02 11:16:57 -0700

    Deleted main.storyboard and manualy created the UIWindow with code.

--- Auto Layout Programmatically/Auto Layout Programmatically/ViewController.swift
+++ Auto Layout Programmatically/Auto Layout Programmatically/ViewController.swift
@@ -29,6 +29,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         view.addSubview(stepOneImageView)
         view.addSubview(descriptionTextView)
         
