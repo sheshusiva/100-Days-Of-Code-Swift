0155e2eb8a07db750d0164248d9ee44d84dcd96b
wells86 <calebrwells@gmail.com>
2017-10-19 20:32:43 -0700

    Getting the basic UI elements done entirely in code.

--- Timer/Timer/ViewController.swift
+++ Timer/Timer/ViewController.swift
@@ -9,11 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let timerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Start Timer!", for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        button.addTarget(self, action: #selector(startTimer), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         navigationItem.title = "Timer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        view.addSubview(timerButton)
+        timerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        timerButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    @objc func startTimer() {
+        print("=== Start the timer!!!")
     }
     
 }
