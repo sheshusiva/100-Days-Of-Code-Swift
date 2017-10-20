3a8fc59725fc8ba17a91f91984c78e2bf767862a
wells86 <calebrwells@gmail.com>
2017-10-19 19:25:45 -0700

    Started a test timer.

---
+++ Timer/Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Caleb Wells on 10/19/17.
+//  Copyright © 2017 Caleb Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        navigationItem.title = "Timer"
+    }
+    
+}
+
