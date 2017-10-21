adc0bba60bb154273e86e82a3c85d4a8ae9f8023
wells86 <calebrwells@gmail.com>
2017-10-20 22:25:25 -0700

    Got a label that updates when I tap the button.

--- Timer/Timer/ViewController.swift
+++ Timer/Timer/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     var timer: Timer!
+    var timeLeft = 0
     
     let timerButton: UIButton = {
         let button = UIButton(type: .system)
@@ -19,6 +20,13 @@ class ViewController: UIViewController {
         button.addTarget(self, action: #selector(startTimerButton), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
+    }()
+    
+    let timeLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .red
+       return label
     }()
 
     override func viewDidLoad() {
@@ -30,21 +38,29 @@ class ViewController: UIViewController {
         view.addSubview(timerButton)
         timerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         timerButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        view.addSubview(timeLabel)
+        timeLabel.text = "Time left: \(timeLeft)"
+        timeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        timeLabel.topAnchor.constraint(equalTo: timerButton.topAnchor, constant: -100).isActive = true
     }
     
     @objc func startTimerButton() {
         print("=== Start the timer!!!")
-        timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(timerIsRunning), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerIsRunning), userInfo: nil, repeats: true)
     }
     
     @objc func timerIsRunning() {
-        print("=== timer is running!")
-        if view.backgroundColor == .white {
-            view.backgroundColor = .yellow
-        } else if view.backgroundColor == .yellow {
-            view.backgroundColor = .white
-        }
+        timeLeft += 1
+        timeLabel.text = "Time left: \(timeLeft)"
+        print("=== time left \(timeLeft)")
+//        if view.backgroundColor == .white {
+//            view.backgroundColor = .yellow
+//        } else if view.backgroundColor == .yellow {
+//            view.backgroundColor = .white
+//        }
         
     }
+    
 }
 
