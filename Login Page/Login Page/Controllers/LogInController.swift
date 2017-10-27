3a6a18e6cae0db17cff16b78d4ce43bca472a252
wells86 <calebrwells@gmail.com>
2017-10-26 23:59:18 -0600

    Starting on button next.

--- Login Page/Login Page/Controllers/LogInController.swift
+++ Login Page/Login Page/Controllers/LogInController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class LogInController: UIViewController {
+    
+    let inputsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 12
+        view.layer.masksToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .blue
+        
+        view.addSubview(inputsContainerView)
+        
+        inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 }
