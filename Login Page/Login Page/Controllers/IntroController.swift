adaddca0e39e240e70869b4581f856fe072b3f1f
wells86 <calebrwells@gmail.com>
2017-10-29 16:30:09 -0600

    added a new image for the intro.

--- Login Page/Login Page/Controllers/IntroController.swift
+++ Login Page/Login Page/Controllers/IntroController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class IntroController: UIViewController {
     
+    let imageView: UIImageView = {
+        let image = UIImageView(image: #imageLiteral(resourceName: "introImage"))
+        image.contentMode = .scaleAspectFill
+        image.layer.cornerRadius = 12
+        image.layer.masksToBounds = true
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
     let loginButton: UIButton = {
         let button = UIButton(type: .system)
         button.backgroundColor = UIColor(red: 13/255, green: 0/255, blue: 204/255, alpha: 1.0)
@@ -28,8 +37,18 @@ class IntroController: UIViewController {
 
         view.backgroundColor = .white
         
+        view.addSubview(imageView)
         view.addSubview(loginButton)
+        
+        setupImageView()
         setupLoginButton()
+    }
+    
+    func setupImageView() {
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: -100).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
     }
     
     func setupLoginButton() {
