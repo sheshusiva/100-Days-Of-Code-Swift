2f8bf5065b947a660721da9c7a50d61f9689b4fd
wells86 <calebrwells@gmail.com>
2017-11-02 21:57:56 -0700

    Working on adding the keyboard show and hide.

--- UI App Idea/UI App Idea/AppDelegate.swift
+++ UI App Idea/UI App Idea/AppDelegate.swift
@@ -17,9 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow()
         window?.makeKeyAndVisible()
-//        let layout = UICollectionViewFlowLayout()
-//        let cardsView = CardsViewController(collectionViewLayout: layout)
-//        window?.rootViewController = UINavigationController(rootViewController: cardsView)
         window?.rootViewController = LoginViewController()
         
         return true
--- UI App Idea/UI App Idea/LoginViewController.swift
+++ UI App Idea/UI App Idea/LoginViewController.swift
@@ -35,7 +35,77 @@ class LoginViewController: UIViewController, UIImagePickerControllerDelegate, UI
         return text
     }()
     
-
+    let nameTextField: UITextField = {
+        let text = UITextField()
+        text.placeholder = "Name"
+        text.translatesAutoresizingMaskIntoConstraints = false
+        return text
+    }()
+    
+    let nameSeparatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.2)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let emailTextField: UITextField = {
+        let text = UITextField()
+        text.placeholder = "Email"
+        text.keyboardType = .emailAddress
+        text.translatesAutoresizingMaskIntoConstraints = false
+        return text
+    }()
+    
+    let emailSeparatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.2)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let passwordTextField: UITextField = {
+        let text = UITextField()
+        text.placeholder = "Password"
+        text.isSecureTextEntry = true
+        text.translatesAutoresizingMaskIntoConstraints = false
+        return text
+    }()
+    
+    let passwordSeparatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.2)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let registerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(red: 255/255, green: 104/255, blue: 179/255, alpha: 1.0)
+        button.setTitle("Register", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(handleRegister), for: .touchUpInside)
+        return button
+    }()
+    
+    let cancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .red
+        button.setTitle("Cancel", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(handleCancel), for: .touchUpInside)
+        return button
+    }()
+    
+    //MARK: ==== View did looad.
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -43,16 +113,28 @@ class LoginViewController: UIViewController, UIImagePickerControllerDelegate, UI
         
         view.addSubview(imageView)
         view.addSubview(textView)
+        view.addSubview(nameTextField)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(registerButton)
+        view.addSubview(cancelButton)
         
         setupImageView()
         setupTextView()
+        setupTextField()
+        setupEmailTextField()
+        setupPasswordTextField()
+        setupRegisterButton()
+        setupCancelButton()
+        
     }
     
+    //MARK: ==== Set up subviews.
     func setupImageView() {
         imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        imageView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -50).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 400).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
         
         view.isUserInteractionEnabled = true
         imageView.isUserInteractionEnabled = true
@@ -62,11 +144,75 @@ class LoginViewController: UIViewController, UIImagePickerControllerDelegate, UI
     
     func setupTextView() {
         textView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        textView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 45).isActive = true
-        textView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -50).isActive = true
-        textView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        textView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 25).isActive = true
+        textView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        textView.heightAnchor.constraint(equalToConstant: 20).isActive = true
     }
     
+    func setupTextField() {
+        nameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        nameTextField.bottomAnchor.constraint(equalTo: textView.bottomAnchor, constant: 80).isActive = true
+        nameTextField.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        nameTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        view.addSubview(nameSeparatorView)
+        nameSeparatorView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        nameSeparatorView.bottomAnchor.constraint(equalTo: nameTextField.bottomAnchor).isActive = true
+        nameSeparatorView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        nameSeparatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    }
+    
+    func setupEmailTextField() {
+        emailTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        emailTextField.bottomAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 40).isActive = true
+        emailTextField.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        emailTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        view.addSubview(emailSeparatorView)
+        emailSeparatorView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        emailSeparatorView.bottomAnchor.constraint(equalTo: emailTextField.bottomAnchor).isActive = true
+        emailSeparatorView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        emailSeparatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    }
+    
+    func setupPasswordTextField() {
+        passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        passwordTextField.bottomAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 40).isActive = true
+        passwordTextField.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        passwordTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        view.addSubview(passwordSeparatorView)
+        passwordSeparatorView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        passwordSeparatorView.bottomAnchor.constraint(equalTo: passwordTextField.bottomAnchor).isActive = true
+        passwordSeparatorView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        passwordSeparatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    }
+    
+    func setupRegisterButton() {
+        registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        registerButton.bottomAnchor.constraint(equalTo: passwordSeparatorView.bottomAnchor, constant: 100).isActive = true
+        registerButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        registerButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    func setupCancelButton() {
+        cancelButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        cancelButton.bottomAnchor.constraint(equalTo: registerButton.bottomAnchor, constant: 66).isActive = true
+        cancelButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        cancelButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    //MARK: ==== Handle UIButtons/Keyboard.
+    @objc func handleRegister() {
+        //
+    }
+    
+    @objc func handleCancel() {
+        //
+    }
+    
+    
+    //MARK: ==== Face detection code!
     @objc func selectPhoto() {
         let picker = UIImagePickerController()
         picker.allowsEditing = true
