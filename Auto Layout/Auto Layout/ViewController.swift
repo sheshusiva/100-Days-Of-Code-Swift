b1f4d7048f9a2bfd0e5e94481880aed3479c9503
wells86 <calebrwells@gmail.com>
2017-11-04 21:00:38 -0700

    set up a UITextView

--- Auto Layout/Auto Layout/ViewController.swift
+++ Auto Layout/Auto Layout/ViewController.swift
@@ -15,19 +15,42 @@ class ViewController: UIViewController {
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
-
+    
+    let textView: UITextView = {
+        let text = UITextView()
+        text.textAlignment = .center
+        text.font = UIFont.boldSystemFont(ofSize: 18)
+        text.text = "Join us today in our fun in games!"
+        text.isEditable = false
+        text.isScrollEnabled = false
+        text.translatesAutoresizingMaskIntoConstraints = false
+        return text
+    }()
+    
+    //MARK: === View did load
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addSubview(imageView)
+        view.addSubview(textView)
+        
         setupImageView()
+        setupTextView()
     }
     
+    //MARK: === Set up layout
     func setupImageView() {
         imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        imageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 72).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+    }
+    
+    func setupTextView() {
+        textView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 50).isActive = true
+        textView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        textView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        textView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     }
 }
 
