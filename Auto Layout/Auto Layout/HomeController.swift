6b5134ac27071fcce4bbdde0f3c5ce71385908e2
wells86 <calebrwells@gmail.com>
2017-11-06 20:56:32 -0800

    Every thing is working.

--- Auto Layout/Auto Layout/HomeController.swift
+++ Auto Layout/Auto Layout/HomeController.swift
@@ -11,30 +11,27 @@ import UIKit
 class HomeController: UIViewController {
     
     let profileButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setImage(#imageLiteral(resourceName: "profile").withRenderingMode(.alwaysOriginal), for: .normal)
-        button.setTitleColor(.mainPink, for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        let button = UIButton(type: .custom)
+        button.setImage(#imageLiteral(resourceName: "profile"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
         button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(handleProfile), for: .touchUpInside)
         return button
     }()
     
     let homeButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setImage(#imageLiteral(resourceName: "home").withRenderingMode(.alwaysOriginal), for: .normal)
-        button.setTitleColor(.mainPink, for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        let button = UIButton(type: .custom)
+        button.setImage(#imageLiteral(resourceName: "home"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
         button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(handleHome), for: .touchUpInside)
         return button
     }()
     
     let messagesButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setImage(#imageLiteral(resourceName: "messages").withRenderingMode(.alwaysOriginal), for: .normal)
-        button.setTitleColor(.mainPink, for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        let button = UIButton(type: .custom)
+        button.setImage(#imageLiteral(resourceName: "messages"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
         button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(handleMessages), for: .touchUpInside)
         return button
@@ -60,7 +57,7 @@ class HomeController: UIViewController {
         
         view.addSubview(buttonsTopStackView)
         
-        buttonsTopStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        buttonsTopStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 12).isActive = true
         buttonsTopStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 12).isActive = true
         buttonsTopStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -12).isActive = true
         buttonsTopStackView.heightAnchor.constraint(equalToConstant: 50).isActive = true
