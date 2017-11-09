4958dc7183c310e76d56f2a2e5a5887af40f0349
wells86 <calebrwells@gmail.com>
2017-11-09 10:54:56 -0800

    Operators

--- Swift 4 Essentials.playground/Contents.swift
+++ Swift 4 Essentials.playground/Contents.swift
@@ -22,3 +22,25 @@ var typeInferred = 5
 var typeExplicit: Int
 // both annotation and inferred
 var typeExplicitAndInferred: Double = 5.5
+
+// Operators!
+
+// Arithmetic
+var math = 5 + 4 - 3 / 2 * 1
+
+// Remainder %
+var hpReminder = 10 % 9
+
+// Compound assignments (+=, -=, *=, /=, 5=)
+var currentMP = 15
+currentMP -= 5
+
+// Comparisons (==, !=, >, <, .=, ,=)
+hp == maxHP
+hp > currentMP
+
+// Ternary operator
+// If hp is less then 50, return 25, if it's not return 50
+var bonusPoints = hp < 50 ? 25 : 50
+
+
