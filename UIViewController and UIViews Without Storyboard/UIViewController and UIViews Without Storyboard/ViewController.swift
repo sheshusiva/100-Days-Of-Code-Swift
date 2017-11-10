fdc0d087881cab51be219dc1aa0d2215bc173284
wells86 <calebrwells@gmail.com>
2017-11-09 20:31:23 -0800

    Added a UILabel.

--- UIViewController and UIViews Without Storyboard/UIViewController and UIViews Without Storyboard/ViewController.swift
+++ UIViewController and UIViews Without Storyboard/UIViewController and UIViews Without Storyboard/ViewController.swift
@@ -16,12 +16,24 @@ class ViewController: UIViewController {
         header.translatesAutoresizingMaskIntoConstraints = false
         return header
     }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "A Title Label in Swift!"
+        label.textAlignment = .center
+        //label.font = UIFont(name: label.font.fontName, size: 27)
+        label.numberOfLines = 0
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
         view.addSubview(headerView)
+        headerView.addSubview(titleLabel)
         
         seUpViewLayout()
     }
@@ -31,6 +43,11 @@ class ViewController: UIViewController {
         headerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
         headerView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 1).isActive = true
         headerView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.15).isActive = true
+        
+        titleLabel.centerXAnchor.constraint(equalTo: headerView.centerXAnchor).isActive = true
+        titleLabel.centerYAnchor.constraint(equalTo: headerView.centerYAnchor).isActive = true
+        titleLabel.widthAnchor.constraint(equalTo: headerView.widthAnchor, multiplier: 0.4).isActive = true
+        titleLabel.heightAnchor.constraint(equalTo: headerView.heightAnchor, multiplier: 0.5).isActive = true
     }
 }
 
