7ac4bff7d2fd5b27ad8d937cf939aa6705270978
wells86 <calebrwells@gmail.com>
2017-11-15 11:33:16 -0800

    ...

--- Why is UIViewController so Important?/Why is UIViewController so Important?/ViewController.swift
+++ Why is UIViewController so Important?/Why is UIViewController so Important?/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let redView: UIView = {
+        let redView = UIView()
+        redView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        redView.backgroundColor = .red
+        redView.layer.cornerRadius = 12
+        redView.layer.masksToBounds = true
+        return redView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .blue
+        
+        view.addSubview(redView)
+        redView.center = view.center
+    }
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        coordinator.animate(alongsideTransition: { (_) in
+            self.redView.center = self.view.center
+        }, completion: nil)
+        
+        print("=== Did rotate device")
     }
 }
 
