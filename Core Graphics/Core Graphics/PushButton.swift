6759a2046918f5bd9678849129e00ac4c7a478be
wells86 <calebrwells@gmail.com>
2017-11-16 20:39:36 -0800

    Set up a UIBezierPath, gave it a starting position (left side of the circle) and draw to the end position (right side of the circle). Then stroke the path outline in white.

--- Core Graphics/Core Graphics/PushButton.swift
+++ Core Graphics/Core Graphics/PushButton.swift
@@ -9,9 +9,50 @@
 import UIKit
 
 class PushButton: UIButton {
+    
+    private struct Constants {
+        static let plusLineWidth: CGFloat = 3.0
+        static let plusButtonScale: CGFloat = 0.6
+        static let halfPointShift: CGFloat = 0.5
+    }
+    
+    private var halfWidth: CGFloat {
+        return bounds.width / 2
+    }
+    
+    private var halfHeight: CGFloat {
+        return bounds.height / 2
+    }
+    
     override func draw(_ rect: CGRect) {
         let path = UIBezierPath(ovalIn: rect)
-        UIColor.green.setFill()
+        #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1).setFill()
         path.fill()
+        
+        // Set up the width and height variables for the horizontal stroke.
+        let plusWidth: CGFloat = min(bounds.width, bounds.height) * Constants.plusButtonScale
+        let halfPlusWidth = plusWidth / 2
+        
+        // Create the path.
+        let plusPath = UIBezierPath()
+        
+        // Set the path's line width to the height of the stroke.
+        plusPath.lineWidth = Constants.plusLineWidth
+        
+        // Move the initial point of the path to the start of the horizontal stroke.
+        plusPath.move(to: CGPoint(
+            x: halfWidth - halfPlusWidth,
+            y: halfHeight))
+        
+        // Add a point to the path at the end of the stroke.
+        plusPath.addLine(to: CGPoint(
+            x: halfWidth + halfPlusWidth,
+            y: halfHeight))
+        
+        // Set the stroke color.
+        UIColor.white.setStroke()
+        
+        // Draw the stroke
+        plusPath.stroke()
     }
 }
