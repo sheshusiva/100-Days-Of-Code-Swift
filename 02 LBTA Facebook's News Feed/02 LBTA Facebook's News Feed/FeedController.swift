161909113fbabe1d937802f316eeb5e461a51bcf
wells86 <calebrwells@gmail.com>
2017-11-30 09:07:25 -0800

    Getting things started.

--- 02 LBTA Facebook's News Feed/02 LBTA Facebook's News Feed/FeedController.swift
+++ 02 LBTA Facebook's News Feed/02 LBTA Facebook's News Feed/FeedController.swift
@@ -2,17 +2,16 @@
 //  FeedController.swift
 //  02 LBTA Facebook's News Feed
 //
-//  Created by Caleb Wells on 11/29/17.
+//  Created by Caleb Wells on 11/30/17.
 //  Copyright © 2017 Caleb Wells. All rights reserved.
 //
 
 import UIKit
 
 class FeedController: UICollectionViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1)
     }
 }
-
